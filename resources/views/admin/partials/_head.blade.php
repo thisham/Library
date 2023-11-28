05d5bdb2e5e23f735fe6621cb9db95847350a930
syaugis <m.syaugishahab@gmail.com>
2023-11-28 10:03:15 +0700

    Change favicon & add select2

--- resources/views/admin/partials/_head.blade.php
+++ resources/views/admin/partials/_head.blade.php
@@ -1,24 +1,37 @@
-
 <!-- Favicon -->
-<link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" />
-<link rel="stylesheet" href="{{asset('css/libs.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/hope-ui.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{asset('css/custom.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{asset('css/dark.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{asset('css/rtl.css?v=1.1.0')}}">
-<link rel="stylesheet" href="{{asset('css/customizer.css?v=1.1.0')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<link rel="stylesheet" href="{{ asset('css/libs.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/hope-ui.css?v=1.1.0') }}">
+<link rel="stylesheet" href="{{ asset('css/custom.css?v=1.1.0') }}">
+<link rel="stylesheet" href="{{ asset('css/dark.css?v=1.1.0') }}">
+<link rel="stylesheet" href="{{ asset('css/rtl.css?v=1.1.0') }}">
+<link rel="stylesheet" href="{{ asset('css/customizer.css?v=1.1.0') }}">
 
 <!-- Fullcalender CSS -->
-<link rel="stylesheet" href="{{asset('vendor/flatpickr/dist/flatpickr.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/flatpickr/dist/flatpickr.min.css') }}">
 {{-- <link rel='stylesheet' href="{{asset('vendor/fullcalendar/core/main.css')}}" />
 <link rel='stylesheet' href="{{asset('vendor/fullcalendar/daygrid/main.css')}}" />
 <link rel='stylesheet' href="{{asset('vendor/fullcalendar/timegrid/main.css')}}" />
 <link rel='stylesheet' href="{{asset('vendor/fullcalendar/list/main.css')}}" />
 <link rel="stylesheet" href="{{asset('vendor/Leaflet/leaflet.css')}}" /> --}}
 
-<link rel="stylesheet" href="{{asset('vendor/aos/dist/aos.css')}}" />
+<link rel="stylesheet" href="{{ asset('vendor/aos/dist/aos.css') }}" />
+
+@if (in_array('select2', $assets ?? []))
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/css/select2.min.css"
+        integrity="sha512-aD9ophpFQ61nFZP6hXYu4Q/b/USW7rpLCQLX6Bi0WJHXNO7Js/fUENpBQf/+P4NtpzNX0jSgR5zVvPOJp+W2Kg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endif
+
 <style>
-    th.hide-search input{
-       display: none;
+    th.hide-search input {
+        display: none;
     }
- </style>
+</style>
--- resources/views/admin/partials/_scripts.blade.php
+++ resources/views/admin/partials/_scripts.blade.php
@@ -1,44 +1,47 @@
 <!-- Backend Bundle JavaScript -->
-<script src="{{ asset('js/libs.min.js')}}"></script>
-@if(in_array('data-table',$assets ?? []))
-<script src="{{ asset('vendor/datatables/buttons.server-side.js')}}"></script>
+<script src="{{ asset('js/libs.min.js') }}"></script>
+@if (in_array('data-table', $assets ?? []))
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
 @endif
-@if(in_array('chart',$assets ?? []))
+@if (in_array('chart', $assets ?? []))
     <!-- apexchart JavaScript -->
-    <script src="{{asset('js/charts/apexcharts.js') }}"></script>
+    <script src="{{ asset('js/charts/apexcharts.js') }}"></script>
     <!-- widgetchart JavaScript -->
-    <script src="{{asset('js/charts/widgetcharts.js') }}"></script>
-    <script src="{{asset('js/charts/dashboard.js') }}"></script>
+    <script src="{{ asset('js/charts/widgetcharts.js') }}"></script>
+    <script src="{{ asset('js/charts/dashboard.js') }}"></script>
 @endif
-
+@if (in_array('select2', $assets ?? []))
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/js/select2.min.js"
+        integrity="sha512-4MvcHwcbqXKUHB6Lx3Zb5CEAVoE9u84qN+ZSMM6s7z8IeJriExrV3ND5zRze9mxNlABJ6k864P/Vl8m0Sd3DtQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+@endif
 <!-- mapchart JavaScript -->
 {{-- <script src="{{asset('vendor/Leaflet/leaflet.js') }} "></script>
 <script src="{{asset('js/charts/vectore-chart.js') }}"></script> --}}
 
-
 <!-- fslightbox JavaScript -->
-<script src="{{asset('js/plugins/fslightbox.js')}}"></script>
-<script src="{{asset('js/plugins/slider-tabs.js') }}"></script>
-<script src="{{asset('js/plugins/form-wizard.js')}}"></script>
+<script src="{{ asset('js/plugins/fslightbox.js') }}"></script>
+<script src="{{ asset('js/plugins/slider-tabs.js') }}"></script>
+<script src="{{ asset('js/plugins/form-wizard.js') }}"></script>
 
 <!-- settings JavaScript -->
-<script src="{{asset('js/plugins/setting.js')}}"></script>
+<script src="{{ asset('js/plugins/setting.js') }}"></script>
 
-<script src="{{asset('js/plugins/circle-progress.js') }}"></script>
-@if(in_array('animation',$assets ?? []))
-<!--aos javascript-->
-<script src="{{asset('vendor/aos/dist/aos.js')}}"></script>
+<script src="{{ asset('js/plugins/circle-progress.js') }}"></script>
+@if (in_array('animation', $assets ?? []))
+    <!--aos javascript-->
+    <script src="{{ asset('vendor/aos/dist/aos.js') }}"></script>
 @endif
 
-@if(in_array('calender',$assets ?? []))
-<!-- Fullcalender Javascript -->
-{{-- {{-- <script src="{{asset('vendor/fullcalendar/core/main.js')}}"></script>
+@if (in_array('calender', $assets ?? []))
+    <!-- Fullcalender Javascript -->
+    {{-- {{-- <script src="{{asset('vendor/fullcalendar/core/main.js')}}"></script>
 <script src="{{asset('vendor/fullcalendar/daygrid/main.js')}}"></script>
 <script src="{{asset('vendor/fullcalendar/timegrid/main.js')}}"></script>
 <script src="{{asset('vendor/fullcalendar/list/main.js')}}"></script>
 <script src="{{asset('vendor/fullcalendar/interaction/main.js')}}"></script> --}}
-<script src="{{asset('vendor/moment.min.js')}}"></script>
-<script src="{{asset('js/plugins/calender.js')}}"></script>
+    <script src="{{ asset('vendor/moment.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/calender.js') }}"></script>
 @endif
 
 <script src="{{ asset('vendor/flatpickr/dist/flatpickr.min.js') }}"></script>
@@ -47,8 +50,8 @@
 
 @stack('scripts')
 
-<script src="{{asset('js/plugins/prism.mini.js')}}"></script>
+<script src="{{ asset('js/plugins/prism.mini.js') }}"></script>
 
 <!-- Custom JavaScript -->
-<script src="{{asset('js/hope-ui.js') }}"></script>
-<script src="{{asset('js/modelview.js')}}"></script>
+<script src="{{ asset('js/hope-ui.js') }}"></script>
+<script src="{{ asset('js/modelview.js') }}"></script>
